2c4252711678b484a5327c60d54163862c205209
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2023-01-09 11:12:02 +0530

    [Sangmeshwar]Add: Ride Method Added and Total Fare Calculation Method Added

---
+++ CabInvoiceGenerator/CabInvoiceGenerator/Ride.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace CabInvoiceGenerator
+{
+    public class Ride
+    {
+        //Variables.
+        public double distance;
+        public int time;
+        /// Parameter Constructor For Setting Data.
+        public Ride(double distance, int time)
+        {
+            this.distance = distance;
+            this.time = time;
+        }
+    }
+}
