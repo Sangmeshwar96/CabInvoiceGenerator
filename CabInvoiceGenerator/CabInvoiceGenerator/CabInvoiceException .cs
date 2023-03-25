3110eb1b06326729ff6dc6057da459cbe0f2cbd8
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2023-03-25 06:47:55 +0530

    [Sangmeshwar] Add: Invoice Generator class added and method to find fare of a ride

---
+++ CabInvoiceGenerator/CabInvoiceGenerator/CabInvoiceException .cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoiceGenerator
+{
+    public class CabInvoiceException : Exception
+    {
+        /// Enum For Exception type.
+        public enum ExceptionType
+        {
+            INVALID_RIDE_TYPE,
+            INVALID_DISTANCE,
+            INVALID_TIME,
+            NULL_RIDES,
+            INVALID_USER_ID
+        }
+        ExceptionType type;
+        /// Parameter Constructor For Setting Exception type And Throwing Exception.
+        public CabInvoiceException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+
+}
